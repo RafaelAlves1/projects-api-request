606bd94ef8fb6577f2b92f45730f93d3e57c96d6
rafael <rafahreispc@gmail.com>
2022-11-11 16:06:30 -0300

    add: screens "Login" and "Register"

--- auth-project/src/Pages/Login/Login.js
+++ auth-project/src/Pages/Login/Login.js
@@ -1,7 +1,86 @@
+import { Box, Button, Container, Input } from '@mui/material';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../services/FirebaseConfig';
 import React from 'react';
+import { toast } from 'react-toastify';
+
+import './Login.modules.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  // async function handleLogin() {
+  //   if (email !== '' && password !== '') {
+  //     await signInWithEmailAndPassword(auth, email, password)
+  //       .then(() => {
+  //         toast.success('Logado com sucesso');
+  //         setEmail('');
+  //         setPassword('');
+  //       })
+  //       .catch((err) => {
+  //         if (err.code === 'auth/invalid-email') {
+  //           toast.error('Email inválido');
+  //         }
+  //         if (err.code === 'auth/wrong-password') {
+  //           toast.error('Senha inválida');
+  //         }
+  //       });
+  //   } else {
+  //     toast.error('Preencha todos os campos');
+  //   }
+  // }
+
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        height: '100vh',
+        padding: '10px',
+      }}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        height="100%"
+        width="100%"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="inputLogin"
+          placeholder="Digite o email"
+        />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="inputLogin"
+          placeholder="Digite sua senha"
+        />
+        <Button
+          variant="contained"
+          // onClick={handleLogin}
+          sx={{
+            width: '100%',
+            margin: '10px 0',
+          }}
+        >
+          Entrar
+        </Button>
+        <Button
+          variant="contained"
+          sx={{
+            width: '100%',
+            marginTop: '10px',
+          }}
+        >
+          Registrar
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default Login;
--- auth-project/src/Pages/Register/Register.js
+++ auth-project/src/Pages/Register/Register.js
@@ -1,7 +1,82 @@
+import { Box, Button, Container, Input } from '@mui/material';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../services/FirebaseConfig';
 import React from 'react';
+import { toast } from 'react-toastify';
+
+import './Register.modules.css';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  // const navigate = useNavigate();
+
+  // async function handleCadastro(e) {
+  //   e.preventDefault();
+  //   if (email !== '' && password !== '') {
+  //     await createUserWithEmailAndPassword(auth, email, password)
+  //       .then(() => {
+  //         toast.success('Cadastrado com sucesso');
+  //         setEmail('');
+  //         setPassword('');
+  //         navigate('/');
+  //       })
+  //       .catch((err) => {
+  //         if (err.code === 'auth/email-already-in-use') {
+  //           toast.error('Email ja cadastrado');
+  //         }
+
+  //         if (err.code === 'auth/weak-password') {
+  //           toast.error('Senha fraca');
+  //         }
+  //       });
+  //   } else {
+  //     toast.error('Preencha todos os campos');
+  //   }
+  // }
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        height: '100vh',
+        padding: '10px',
+      }}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        height="100%"
+        width="100%"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="inputRegister"
+          placeholder="Digite o email"
+        />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="inputRegister"
+          placeholder="Digite sua senha"
+        />
+        <Button
+          variant="contained"
+          // onClick={handleCadastro}
+          sx={{
+            width: '100%',
+            margin: '10px 0',
+          }}
+        >
+          Cadastrar
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default Register;
