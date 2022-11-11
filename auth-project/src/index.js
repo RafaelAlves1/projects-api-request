1b8ec566f937b5e3a81ce2b7367dc09c34197b24
rafael <rafahreispc@gmail.com>
2022-11-11 13:49:33 -0300

    feat: added "ToastfyContainer" and "CssBaseline" in "index.js"

--- auth-project/src/index.js
+++ auth-project/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { CssBaseline } from '@mui/material';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
+    <CssBaseline />
   </React.StrictMode>,
 );
